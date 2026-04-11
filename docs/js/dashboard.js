4ad6a588ad892bcaca807787a393be65a9ccc8ad
mcasrom <mcasrom@gmail.com>
2026-04-11 14:52:27 +0200

    Dashboard funcional: render básico

--- docs/js/dashboard.js
+++ docs/js/dashboard.js
@@ -10,7 +10,7 @@ window.onload = () => {
     let html = "<h2>Riesgo por país</h2>";
     for (const pais in PAISES_INFO) {
         const info = PAISES_INFO[pais];
-        html += `<div class="pais"><strong>${pais}</strong> — Riesgo: ${info.riesgo}</div>`;
+        html += `<div><strong>${pais}</strong> — Riesgo: ${info.riesgo}</div>`;
     }
     document.getElementById("riesgo").innerHTML = html;
 };
